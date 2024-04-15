ee76b089cdb4655f51e835e927f2ff10077f90d5
asobrab <ramonfernandesbarbosa13@gmail.com>
2024-04-15 19:03:54 -0300

    inicia projeto

---
+++ src/main/java/academy/wakanda/DesafioMedioJavaBE/DesafioMedioJavaBeApplication.java
@@ -0,0 +1,13 @@
+package academy.wakanda.DesafioMedioJavaBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMedioJavaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMedioJavaBeApplication.class, args);
+	}
+
+}
