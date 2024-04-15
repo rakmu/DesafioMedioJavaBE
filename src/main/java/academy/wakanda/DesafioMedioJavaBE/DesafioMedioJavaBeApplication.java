9d793f3c4afeeae9b6f652df6b0c26dfb1d20130
asobrab <ramonfernandesbarbosa13@gmail.com>
2024-04-15 19:42:13 -0300

    configura MongoDB, Teste ok!

--- src/main/java/academy/wakanda/DesafioMedioJavaBE/DesafioMedioJavaBeApplication.java
+++ src/main/java/academy/wakanda/DesafioMedioJavaBE/DesafioMedioJavaBeApplication.java
@@ -2,9 +2,17 @@ package academy.wakanda.DesafioMedioJavaBE;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController("/")
 public class DesafioMedioJavaBeApplication {
+
+	@GetMapping
+	public String getHomeTeste() {
+		return "Pessoa Endereco - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioMedioJavaBeApplication.class, args);
